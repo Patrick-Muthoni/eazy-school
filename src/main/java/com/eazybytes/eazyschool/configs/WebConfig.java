22d07c377f141c43d6a89f12e8f01048ab6519d7
Padie-Muthoni <padiemaina65@gmail.com>
2023-02-01 23:10:47 +0300

    using WebMvcConfigurer

--- src/main/java/com/eazybytes/eazyschool/configs/WebConfig.java
+++ src/main/java/com/eazybytes/eazyschool/configs/WebConfig.java
@@ -1,4 +1,14 @@
 package com.eazybytes.eazyschool.configs;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/courses").setViewName("courses");
+    }
 }
