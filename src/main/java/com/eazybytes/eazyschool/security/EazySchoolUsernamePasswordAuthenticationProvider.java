a4f440fc0477cf317f2a6c36ecfbec0a4f4df42d
Padie-Muthoni <padiemaina65@gmail.com>
2023-03-14 14:43:24 +0300

    custom authentication from db

---
+++ src/main/java/com/eazybytes/eazyschool/security/EazySchoolUsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,54 @@
+package com.eazybytes.eazyschool.security;
+
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Roles;
+import com.eazybytes.eazyschool.repository.PersonRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class EazySchoolUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public EazySchoolUsernamePasswordAuthenticationProvider(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
+        this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Person person = personRepository.readByEmail(email);
+        if (person != null && person.getPersonId() > 0
+                && passwordEncoder.matches(pwd, person.getPwd())) {
+            return new UsernamePasswordAuthenticationToken(person.getName(), null, getGrantedAuthorities(person.getRole()));
+        } else {
+            throw new BadCredentialsException("Invalid Credentials");
+        }
+    }
+
+    private Collection<? extends GrantedAuthority> getGrantedAuthorities(Roles role) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/eazybytes/eazyschool/service/PersonService.java
+++ src/main/java/com/eazybytes/eazyschool/service/PersonService.java
@@ -5,6 +5,8 @@ import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.model.Roles;
 import com.eazybytes.eazyschool.repository.PersonRepository;
 import com.eazybytes.eazyschool.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,16 +14,19 @@ public class PersonService {
 
     private final PersonRepository personRepository;
     private final RolesRepository rolesRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public PersonService(PersonRepository personRepository, RolesRepository rolesRepository) {
+    public PersonService(PersonRepository personRepository, RolesRepository rolesRepository, PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
         this.rolesRepository = rolesRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public boolean createNewPerson(Person person) {
         boolean isSaved = false;
         Roles role = rolesRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
         person.setRole(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
         person = personRepository.save(person);
         if (person.getPersonId() >0) {
             isSaved = true;
